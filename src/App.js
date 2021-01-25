fe4a3c9c6a405f0bdbcfc53a642b3c968abca83c
Ammar <ammar.h.d@hotmail.com>
2021-01-25 05:47:55 +0100

    implementing the header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React from "react";
 import "./App.css";
+import "./components/components.css";
+import WatchList from "./components/watchList";
+import Header from "./components/header";
+import Watched from "./components/watched";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">heeey</div>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <WatchList />
+        </Route>
+        <Route path="/Watched">
+          <Watched />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/header/index.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [watchListButton, setWatchListButton] = useState("primary");
+  const [watchedButton, setWatchedButton] = useState("default");
+
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const watchListClicked = () => {
+    setWatchListButton((prevColor) => "primary");
+    setWatchedButton((prevColor) => "default");
+  };
+  const watchedClicked = () => {
+    setWatchedButton((prevColor) => "primary");
+    setWatchListButton((prevColor) => "default");
+  };
+  return (
+    <div className="container  align-items-center">
+      <div className=" row mt-4 mb-4">
+        <div className="col-4">
+          <Link to="/" className="header a">
+            <h1>WatchList</h1>
+          </Link>
+        </div>
+
+        <div className="col-6 mt-2">
+          <span>
+            <Link to="/" className="a">
+              <Button
+                variant="contained"
+                color={watchListButton}
+                onClick={watchListClicked}
+                className="Btn"
+              >
+                WatchList
+              </Button>
+            </Link>
+          </span>
+          <span>
+            <Link to="/Watched" className="a">
+              <Button
+                variant="contained"
+                color={watchedButton}
+                onClick={watchedClicked}
+                className="Btn"
+              >
+                Watched
+              </Button>
+            </Link>
+          </span>
+        </div>
+
+        <div className="col mt-1">
+          <Button variant="contained" color="secondary" onClick={togglePopup}>
+            + ADD
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
