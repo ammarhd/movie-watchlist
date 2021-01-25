15246f81fab7956047ae4b152b4fe83e43ab6642
Ammar <ammar.h.d@hotmail.com>
2021-01-25 07:49:36 +0100

    sitting the store and the persist gate

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { store, persistor } from "./Store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
 
   document.getElementById("root")
 );
